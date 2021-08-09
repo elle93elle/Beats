a534d4f8577e7338726c4c924048187921d6c7e8
Elina <elle93elle@gmail.com>
2021-08-10 00:54:49 +0500

    fullscreen menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,13 @@
 const openBurger = document.querySelector(".hamburger");
+const fullscreenMenu = document.querySelector(".fullscreen-menu");
+const close = document.querySelector(".fullscreen-menu__close");
 
 openBurger.addEventListener ("click", e => {
-    const fullscreenMenu = document.querySelector (".fullscreen-menu")
-})
+    e.preventDefault();
+    fullscreenMenu.classList.add("open");
+});
+
+close.addEventListener ("click", e => {
+    e.preventDefault();
+    fullscreenMenu.classList.remove("open");
+});
